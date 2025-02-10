119473a039e29e837b3ab40003b0d5854887da50
SmDUm <samueldemoraesdelgado2005@gmail.com>
2025-02-10 06:54:04 -0300

    Exercícios

--- meu-projeto/src/components/List.js
+++ meu-projeto/src/components/List.js
@@ -5,12 +5,12 @@ function List() {
         <>
             <h1>Minha Lista</h1>
             <ul>
-                <Item marca="Ferrari" />
-                <Item marca="Fiat" />
-                <Item marca="Ford" />
-                <Item marca="Chevrolet" />
-                <Item marca="Volkswagen" />
-                <Item marca="Renault" />
+                <Item marca="Ferrari" ano_lancamento={1985}/>
+                <Item marca="Fiat" ano_lancamento = {1964} />
+                <Item marca="Ford"  ano_lancamento = {2003}/>
+                <Item marca="Chevrolet" ano_lancamento = {1981}/>
+                <Item marca="Volkswagen" ano_lancamento = {2014}/>
+                <Item marca="Renault" ano_lancamento = {2004}/>
             </ul>
         </>
     )
